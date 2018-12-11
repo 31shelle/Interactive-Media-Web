9b02f403ad1ea19ee43dc60ce21d4e0bb2e86325
Michelle <3149008@student.ocadu.ca>
2018-12-11 13:46:38 -0500

    connections

---
+++ assignment4/js/script.js
@@ -0,0 +1,124 @@
+// $speed=700
+
+$("#section0").click(function(){
+
+	$(window).scrollTo("#to1",700,{offset:{top:-400,left:-800}});
+})
+
+$("#section1").click(function(){
+
+	$(window).scrollTo("#to2",700,{offset:{top:-300, left:-880}});
+})
+
+$("#section2").click(function(){
+
+	$(window).scrollTo("#sh3",700,{offset:{top:-450, left:-500}});
+})
+
+$("#section3").click(function(){
+
+	$(window).scrollTo("#sh4",700, {offset:{top:-450, left:-300}});
+})
+
+$("#section4").click(function(){
+
+	$(window).scrollTo("#to5",700, {offset:{top:-250, left:-650}});
+})
+
+$("#section5").click(function(){
+
+	$(window).scrollTo("#to6",700, {offset:{top:-320, left:-910}});
+})
+
+$("#section6").click(function(){
+
+	$(window).scrollTo("#sh7",700, {offset:{top:-430, left:-1100}});
+})
+
+$("#section7").click(function(){
+
+	$(window).scrollTo("#sh8",700, {offset:{top:-350, left:-555}});
+})
+
+$("#section8").click(function(){
+
+	$(window).scrollTo("#to9",700,{offset:{top:-350, left:-500}});
+})
+
+$("#section9").click(function(){
+
+	$(window).scrollTo("#to10",700,{offset:{top:-600, left:-480}});
+})
+
+$("#section10").click(function(){
+
+	$(window).scrollTo("#sh11",700,{offset:{top:-300, left:-450}});
+})
+
+$("#section11").click(function(){
+
+	$(window).scrollTo("#to12",700, {offset:{top:-350, left:-500}});
+})
+
+$("#section12").click(function(){
+
+	$(window).scrollTo("#to13",700, {offset:{top:-300, left:-700}});
+})
+
+$("#section13").click(function(){
+
+	$(window).scrollTo("#sh14",700, {offset:{top:-300, left:-500}});
+})
+
+$("#section14").click(function(){
+
+	$(window).scrollTo("#sh15",700, {offset:{top:-400, left:-1100}});
+})
+
+$("#section15").click(function(){
+
+	$(window).scrollTo("#sh16",700, {offset:{top:-330, left:-750}});
+})
+
+$("#section16").click(function(){
+
+	$(window).scrollTo("#to17",700,{offset:{top:-250,left:-550}});
+})
+
+$("#section17").click(function(){
+
+	$(window).scrollTo("#to18",700,{offset:{top:-400, left:-700}});
+})
+
+$("#section18").click(function(){
+
+	$(window).scrollTo("#sh19",700,{offset:{top:-350, left:-750}});
+})
+
+$("#section19").click(function(){
+
+	$(window).scrollTo("#sh20",700, {offset:{top:-350, left:-400}});
+})
+
+$("#section20").click(function(){
+
+	$(window).scrollTo("#to21",700, {offset:{top:-400, left:-420}});
+})
+
+$("#section21").click(function(){
+
+	$(window).scrollTo("#sh22",700, {offset:{top:-350, left:-870}});
+})
+
+$("#section22").click(function(){
+
+	$(window).scrollTo("#sh23",700, {offset:{top:-380, left:-350}});
+})
+
+$("#section23").click(function(){
+
+	$(window).scrollTo("#to24",700, {offset:{top:-450, left:-520}});
+})
+
+
+
